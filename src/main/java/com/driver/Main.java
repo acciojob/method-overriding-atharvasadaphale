077b53ba487d94f4df63fea87a78a238ad3a4e9b
unknown <atharvasadaphale22@gmail.com>
2023-01-13 14:48:41 +0530

    override sol 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,18 +4,19 @@ public class Main {
     public static void main(String[] args) {
         B obj = new B();
         String res = obj.meth();
+        //System.out.println(res);
     }
-}r
+}
 class A {
-        public String meth() {
-            return "Invoking method from class A";
-        }
+    public String meth() {
+        return "Invoking method from class A";
     }
-
+}
     class B extends A {
         @Override
         public String meth() {
             return "Method is overridden in Extendend class B";
         }
     }
+
 
