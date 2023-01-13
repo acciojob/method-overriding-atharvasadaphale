fb9472774e6eacf43aaa09f5871f3f8e311ef998
unknown <atharvasadaphale22@gmail.com>
2023-01-13 14:51:53 +0530

    sol override 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         B obj = new B();
         String res = obj.meth();
-        //System.out.println(res);
+        System.out.println(res);
     }
 }
 class A {
